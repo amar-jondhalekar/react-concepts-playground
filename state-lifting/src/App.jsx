997a40850b23a7df9be599a5b427f0a339e11a58
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-01 09:55:11 +0530

    created state, manage state and changed state to sync states to all chid

--- state-lifting/src/App.jsx
+++ state-lifting/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
+import Card from './components/Card';
 
 function App() {
-
+  //create state
+  const [name, setName] = useState('');
+  //manage state
+  //change state
+  //sync states to all child 
   return (
     <>
-      
+      <div>
+        <Card name={name} setName={setName}></Card>
+        <p>I am inside Parent Component and value of name is: {name}</p>
+      </div>
     </>
   )
 }
