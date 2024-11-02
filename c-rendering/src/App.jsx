32823612f2811427ac69bcf8b8d569359da958fc
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-02 09:54:27 +0530

    conditional rendering using if-else, ternary operator, logical AND, Early return

--- c-rendering/src/App.jsx
+++ c-rendering/src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LoginBtn from './components/LoginBtn';
+import LogoutBtn from './components/LogoutBtn';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[isLoggedIn, setLoggedIn] = useState(false);
 
-  return (
-    <>
+  if(!isLoggedIn){
+    return(
+      <LoginBtn/>
+    )
+  }
+
+  return(
+    <div>
+      <h1> Welcome Everyone to Campuslight Web Dev course </h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* Using AND operator */}
+        {isLoggedIn && <LogoutBtn/>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
+
+  // return(
+  //   <div>
+  //     {/* Using ternary operator */}
+  //     {isLoggedIn ? <LogoutBtn/> : <LoginBtn/>}
+  //   </div>
+  // )
+
+  // Using if-else loop 
+  // if(isLoggedIn){
+  //   return (
+  //     <LogoutBtn/>
+  //   )
+  // }else{
+  //   return(
+  //     <LoginBtn/>
+  //   )
+  // }
 }
 
 export default App
