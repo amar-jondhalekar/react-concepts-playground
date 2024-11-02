4dd63e410ee96bf91113fbf6d6fe8d65d0c16563
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-02 10:42:33 +0530

    changes in the onclick function

--- event-handling/src/App.jsx
+++ event-handling/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Button from './components/Button';
 
 function App() {
-  function handleClick(){
-    alert('You cliked this button!');
-  }
+  // function handleClick(){
+  //   alert('You cliked this button!');
+  // }
   return (
     <>
-    <Button clickMe={handleClick}></Button>
+    {/* <button onClick={alert('clicked button')}>Click Me!</button> */}
+    {/* <button onClick={() => {alert('button clicked!')}}>click me!</button> */}
+    {/* <Button clickMe={handleClick}></Button> */}
     </>
   )
 }
