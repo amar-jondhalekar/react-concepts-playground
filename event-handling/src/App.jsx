3ff3e87fb7ae37848905356b4c32f5d0e963600c
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-02 10:54:14 +0530

    added ounting state varable to the button element

--- event-handling/src/App.jsx
+++ event-handling/src/App.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.css'
 import Button from './components/Button';
 
 function App() {
-  // function handleClick(){
-  //   alert('You cliked this button!');
-  // }
+  const[count, setCount] = useState(0);
+
+  function CountingNumber(){
+    setCount(count + 1);
+    console.log(count);
+  }
+  function DecreaseNumber(){
+    setCount(count - 1);
+    console.log(count);
+  }
+  function handleClick(){
+    alert('You cliked this button!');
+  }
   return (
     <>
     {/* <button onClick={alert('clicked button')}>Click Me!</button> */}
-    <button onClick={() => {alert('button clicked!')}}>click me!</button>
-    {/* <Button clickMe={handleClick}></Button> */}
+    {/* <button onClick={() => {alert('button clicked!')}}>click me!</button> */}
+    <Button clickMe={handleClick}></Button>
+    <button onClick={CountingNumber}> Increse Number: {count}</button>
+    <button onClick={DecreaseNumber}> Decrease Number: {count} </button>
     </>
   )
 }
